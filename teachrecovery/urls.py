4a7b547183af3d7131bbf3a193575644376e8575
Jed <jed2161@kodos.ccnmtl.columbia.edu>
2014-09-10 09:11:20 -0400

    a ddign in login_required

--- teachrecovery/urls.py
+++ teachrecovery/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.views.generic import TemplateView
 from teachrecovery.main import views
